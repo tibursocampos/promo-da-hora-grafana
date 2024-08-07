688228a118c243948e091fb4659a8a3497d7a75f
Raphael Campos <raphael_pcampos@outlook.com>
2024-08-06 21:38:11 -0300

    alteração caminho

--- loki/entrypoint.sh
+++ loki/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Inicie o Loki com o arquivo de configuração correto
 echo "Iniciando o Loki com o arquivo de configuração correto"
-exec /usr/bin/loki -config.file=/etc/loki/loki-config.yml
+exec /usr/bin/loki -config.file=/etc/loki/local-config.yaml
