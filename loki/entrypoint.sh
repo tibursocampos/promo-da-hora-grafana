54b01c2a372db4c5c967cdbbbdcda5bd78e4e433
Raphael Campos <raphael_pcampos@outlook.com>
2024-08-06 21:26:39 -0300

    mensagem

--- loki/entrypoint.sh
+++ loki/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # Inicie o Loki com o arquivo de configuração correto
+echo "Iniciando o Loki com o arquivo de configuração correto"
 exec /usr/bin/loki -config.file=/etc/loki/loki-config.yml
